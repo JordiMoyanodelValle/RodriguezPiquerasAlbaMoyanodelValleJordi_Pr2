27665636ebecc58f3d9aa762684b657c8482953e
JordiMoyanodelValle <jmoyande15@alumnes.ub.edu>
2025-03-24 12:08:12 +0100

    Comiited

--- src/prog2/model/allotjament/LlistaAllotjaments.java
+++ src/prog2/model/allotjament/LlistaAllotjaments.java
@@ -2,29 +2,55 @@ package prog2.model.allotjament;
 
 import prog2.vista.ExcepcioCamping;
 
+import java.util.ArrayList;
+
 public class LlistaAllotjaments implements InLlistaAllotjaments {
+    ArrayList<Allotjament> llistaallotjaments = new ArrayList<Allotjament>();
 
     public void afegirAllotjament(Allotjament allotjament) throws ExcepcioCamping {
-
+        llistaallotjaments.add(allotjament);
     }
 
     public void buidar() {
-
+        llistaallotjaments.clear();
     }
 
     public String llistarAllotjaments(String estat) throws ExcepcioCamping {
-        return "";
+        boolean esOperatiu = (estat.equals("Operatiu"));
+        StringBuffer sortida = new StringBuffer();
+        for (Allotjament allotjament : llistaallotjaments) {
+            if (allotjament.isOperatiu() == esOperatiu || estat.equals("Tots")){
+                sortida.append(" "+ allotjament.getNom() + " ");
+            }
+        }
+        return sortida.toString();
     }
 
     public boolean containsAllotjamentOperatiu() {
+        for (Allotjament allotjament : llistaallotjaments) {
+            if (allotjament.isOperatiu()) {
+                return true;
+            }
+        }
         return false;
     }
 
     public boolean contains(Allotjament allotjament) {
+        for (Allotjament allotjament1 : llistaallotjaments) {
+            if (allotjament.getNom().equals(allotjament1.getNom())) {
+                return true;
+            }
+        }
+
         return false;
     }
 
     public Allotjament getAllotjament(String nom) throws ExcepcioCamping {
+        for (Allotjament allotjament : llistaallotjaments) {
+            if (allotjament.getNom().equals(nom)) {
+                return allotjament;
+            }
+        }
         return null;
     }
 }
